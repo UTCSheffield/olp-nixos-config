e48e2957b561a075ccb2aba3d5939c1c5f9c0232
Sean Outram <outramsean0@gmail.com>
2024-10-22 15:21:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ if [ $config ]; then
   echo "Setup Nixos"
   mkdir -p /mnt/etc
   mkdir -p /mnt/etc/nixos
+  nix-env -iA nixos.git
   git clone https://github.com/UTCSheffield/olp-nixos-config /mnt/etc/nixos
   
   nixos-install --flake .#$config
