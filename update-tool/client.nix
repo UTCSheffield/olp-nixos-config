b21bcd7610efd373f74f9ddc35d074cf72869f01
Sean Outram <outramsean0@gmai.com>
2024-11-07 11:33:41 +0000

    Add systemd service

--- update-tool/client.nix
+++ update-tool/client.nix
@@ -1,17 +1,25 @@
-{ rustPlatform, lib }:
+{ config, pkgs }:
 
-rustPlatform.buildRustPackage rec {
+let client = rustPlatform.buildRustPackage rec {
   pname = "update-tool-client";
   version = "1.0.0";
 
   src = ./client;  # Use current directory as source
-
   cargoHash = "sha256-g6M3SZXk5ReUNi1MHfLHE3CtDy9P41ZofTmhAekI0LQ=";
-
-  meta = {
-    description = "Update Client";
-    homepage = "https://github.com/UTCSheffield/olp-nixos-config";
-    license = lib.licenses.unlicense;
-    maintainers = [ ];
-  };
-}
+  subPackages = ["client"];
+};
+in
+{
+  config = {
+    systemd.services.updateClient = {
+      description = "Update tool Client";
+      serviceConfig = {
+      ExecStart = "${client}/bin/client";
+      User = "root";
+      Restart = "on-failure";
+      RestartSec = 10;
+      StartLimitIntervalSec = 30;
+      StartLimitBurst = 3;
+    };
+    wantedBy = [ "multi-user.target" ];
+  }
