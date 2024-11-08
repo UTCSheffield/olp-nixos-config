2421cb1c3646beded69e06fe26f5d3939d790086
Sean Outram <outramsean0@gmail.com>
2024-11-08 12:14:03 +0000

    Maybe fix fs issues

--- update-tool/client/src/utils/git.rs
+++ update-tool/client/src/utils/git.rs
@@ -6,7 +6,7 @@ pub fn git_get_latest_hash(directory: &str) -> String {
     let cmd = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .current_dir(directory)
-        .output()
+        .spawn()
         .expect("Failed to execute command");
     let output = String::from_utf8_lossy(cmd.stdout.as_slice()).into_owned();
     return output;
@@ -16,7 +16,7 @@ pub fn git_pull(directory: &str) -> bool {
     let cmd = Command::new("git")
         .args(&["pull"])
         .current_dir(directory)
-        .output()
+        .spawn()
         .expect("Failed to execute command");
     let output = String::from_utf8_lossy(cmd.stdout.as_slice()).into_owned();
     // We check if the command output contains that string, if it does, we know the pull actually pulled something
