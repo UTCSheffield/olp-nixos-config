25f3dacb044ae3480dcdfa266ee827fcbb018ddf
Sean Outram <outramsean0@gmail.com>
2024-12-11 14:17:47 +0000

    Maybe actually make it work

---
+++ update-tool/client/src/utils/nixos.rs
@@ -0,0 +1,9 @@
+pub fn rebuild_system(directory: &str) {
+    let cmd = Command::new("nixos-rebuild")
+    .args(&["switch", "--flake", ".#makerlab-3040"])
+    .current_dir(directory) // Directory: /etc/nixos
+    .output()
+    .expect("Failed to execute command");
+    let output = String::from_utf8_lossy(cmd.stdout.as_slice()).into_owned();
+    return output;
+}
