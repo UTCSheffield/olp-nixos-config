adcd6ecec1f2970ab8d556e18fdee859bb63f904
Sean Outram <outramsean0@gmail.com>
2024-12-11 13:48:46 +0000

    Update currentVersion.ts

--- update-tool/server/src/responses/update/currentVersion.ts
+++ update-tool/server/src/responses/update/currentVersion.ts
@@ -2,6 +2,7 @@ import { container } from "../../lib/container.js";
 import { logger } from "../../lib/logger.js";
 
 export default async function currentVersion(val: string, id: number) {
+    console.log(container.latestGitCommitHash)
     if (container.latestGitCommitHash !== val) {
         logger.debug(`[${id}] update to ${container.latestGitCommitHash} available`);
         return {
