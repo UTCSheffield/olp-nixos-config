c4d3e2b7ef22a88351dfa1a1ac93a8573654279f
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-01-23 12:44:22 +0000

    Create makerlab-lite-core.nix

---
+++ shared/makerlab-lite-core.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../programs/update-client.nix
+  ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = lib.mkForce "uk";
+    useXkbConfig = true;
+  };
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    gh
+    emacs
+    supercollider
+    python310Full
+    vscode
+    chromium
+    tk
+    python310Packages.keyboard
+  ];
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "24.05";
+}
