9a1508c388330c21f609193eee15a5a9904f834e
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-01-23 13:00:48 +0000

    Update makerlab-lite-userland.nix

--- shared/makerlab-lite-userland.nix
+++ shared/makerlab-lite-userland.nix
@@ -7,7 +7,7 @@
     enable = true;
     xkb.layout = "gb";
   };
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.lxqt.enable = true;
   services.printing.enable = true; # Enables printing
 }
