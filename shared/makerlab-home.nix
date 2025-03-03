52a4f1888e9b83452e3c4270a3e6767541fb12b0
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-03-03 17:13:10 +0000

    Update makerlab-home.nix

--- shared/makerlab-home.nix
+++ shared/makerlab-home.nix
@@ -5,9 +5,10 @@
   home.homeDirectory = "/home/makerlab";
 
   home.file.".local/share/OpenSCAD/libraries/BOSL2" = {
-    source = builtins.fetchGit {
+    source = pkgs.fetchgit {
       url = "https://github.com/BelfrySCAD/BOSL2";
       ref = "4ce427a8a38786e5f74b728c1e33d9fe7d4904d2";
+      sha256 = "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed";
     };
   };
 
