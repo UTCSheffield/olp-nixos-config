22a7a0f5189833f89d651e9dd99a61c28081c404
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-03-03 09:22:13 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   description = "OLP NixOS config";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs = { self, nixpkgs, home-manager, ... }@attrs: {
     nixosConfigurations = {
       makerlab-3040 = nixpkgs.lib.nixosSystem {
@@ -13,17 +15,25 @@
         modules = [
           ./hardware/dell-3040.nix
           ./machines/makerlab-client.nix
+
+          # Add Home-Manager as a NixOS module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.makerlab = import ./home.nix;
+          }
         ];
       };
+
       iso = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = attrs;
         modules = [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
           ./machines/iso.nix
-        ];	
+        ];
       };
     };
   };
-}  
-   
+}
