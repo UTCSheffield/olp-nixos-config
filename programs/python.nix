fb1e22359c79d4d5f2c754f104e86ab219816f89
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-03-04 09:32:15 +0000

    Update python.nix

--- programs/python.nix
+++ programs/python.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... }:
-
+let solidPythonFull = pkgs.callPackage ../update-tool/client.nix {};
+in
 {
   environment.systemPackages = with pkgs; [
     (python312Full.withPackages (python-pkgs: with python-pkgs; [
-      
+      solidPythonFull
     ]))
   ];
 }
