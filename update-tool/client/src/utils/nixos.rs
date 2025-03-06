8d4c6378040f3a5f57b76fb613fbb1376ddabd09
Sean Outram <outramsean0@gmail.com>
2025-03-06 16:20:02 +0000

    Update nixos.rs (#34)

--- update-tool/client/src/utils/nixos.rs
+++ update-tool/client/src/utils/nixos.rs
@@ -3,8 +3,7 @@ use std::process::Command;
 pub fn rebuild_system(directory: &str) -> String {
     println!("Current Path {}", directory);
     let cmd = Command::new("nixos-rebuild")
-    .args(&["switch", "--flake", ".#makerlab-3040"])
-    .current_dir(directory) // Directory: /etc/nixos
+    .args(&["switch", "--flake", "/etc/nixos#makerlab-3040"])
     .output()
     .expect("Failed to execute command");
     let output = String::from_utf8_lossy(cmd.stdout.as_slice()).into_owned();
