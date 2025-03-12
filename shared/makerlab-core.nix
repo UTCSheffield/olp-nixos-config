51f27eb3b4eb638c5c13f117c76820f95acea2ea
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-03-12 15:42:24 +0000

    Update makerlab-core.nix

--- shared/makerlab-core.nix
+++ shared/makerlab-core.nix
@@ -4,6 +4,7 @@
   imports = [
     ../programs/update-client.nix
     ../programs/python.nix
+    ./makerlab-home.nix
   ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
