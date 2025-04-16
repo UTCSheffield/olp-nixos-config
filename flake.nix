a3ac39a18192637d0402e845647995f7196017eb
iLikeToCode <archie@discoverystem.org.uk>
2025-04-16 16:22:24 +0100

    Initial AD Config

--- flake.nix
+++ flake.nix
@@ -4,8 +4,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    himmelblau.url = "github:himmelblau-idm/himmelblau/main";
+    himmelblau.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, home-manager, ... }@attrs: {
+    nixosModules.azureEntraId = {
+        imports = [ himmelblau.nixosModules.himmelblau ];
+        services.himmelblau = {
+            enable = true;
+            settings = {
+                domains = ["utcsheffield.org.uk"];
+                local_groups = [ "wheel" "docker" ];
+            };
+        };
+    };
     nixosConfigurations = {
       makerlab-3040 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -13,6 +25,7 @@
         modules = [
           ./hardware/dell-3040.nix
           ./machines/makerlab-client.nix
+          self.nixosModules.azureEntraId
         ];
       };
       iso = nixpkgs.lib.nixosSystem {
