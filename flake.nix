983cfe067f3af67ff5e3c29d7ed8406015eeddd0
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-04-16 21:28:36 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,16 +4,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    himmelblau.url = "github:himmelblau-idm/himmelblau/main";
+    himmelblau.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, home-manager, ... }@attrs: {
+  outputs = { self, nixpkgs, home-manager, himmelblau, ... }@attrs: {
+    nixosModules.azureEntraId = {
+          imports = [ himmelblau.nixosModules.himmelblau ];
+          services.himmelblau = {
+              enable = true;
+              settings = {
+                  domains = ["utcsheffield.org.uk"];
+                  #pam_allow_groups = [ "ENTRA-GROUP-GUID-HERE" ];
+                  local_groups = [ "wheel" "docker" ];
+              };
+          };
+      };
     nixosConfigurations = {
       makerlab-3040 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = attrs;
         modules = [
+          self.nixosModules.azureEntraId
           ./hardware/dell-3040.nix
           ./machines/makerlab-client.nix
-          ./shared/entra.nix
         ];
       };
       iso = nixpkgs.lib.nixosSystem {
