785acf8abb3782f1ad521529169afaf50809acd4
iLikeToCode <archie@archiesbytes.xyz>
2026-01-09 16:26:46 +0000

    add bosl2 hash

--- programs/openscad.nix
+++ programs/openscad.nix
@@ -9,7 +9,7 @@
           owner = "BelfrySCAD";
           repo = "BOSL2";
           rev = "6ce86cd8a4a29a87444052dffb8704c2d90fd5a4";
-          sha256 = "0000000000000000000000000000000000000000000000000000";
+          sha256 = "sha256-eO1DV/xrC2PuolSX4nM8t6B4F1hDQjjuqVH20hiUdh4=";
           name = "BOSL2";
         }
       ];
