542d487b70f8b6d18444459078a090377cafb357
iLikeToCode <archie@archiesbytes.xyz>
2026-01-09 16:29:42 +0000

    openscad: use correct makeWrapper

--- programs/openscad-with-extensions.nix
+++ programs/openscad-with-extensions.nix
@@ -15,18 +15,15 @@ in pkgs.runCommand "openscad-with-extensions" {
 } ''
   mkdir -p $out/bin $out/libraries $out/share/pixmaps $out/share/applications
 
-  # link icons and desktop files
   ln -sT ${pkgs.openscad}/share/pixmaps/vs${pkgs.openscad.meta.mainProgram}.png $out/share/pixmaps/vs${pkgs.openscad.meta.mainProgram}.png
   ln -sT ${pkgs.openscad}/share/applications/${pkgs.openscad.meta.mainProgram}.desktop $out/share/applications/${pkgs.openscad.meta.mainProgram}.desktop
   ln -sT ${pkgs.openscad}/share/applications/${pkgs.openscad.meta.mainProgram}-url-handler.desktop $out/share/applications/${pkgs.openscad.meta.mainProgram}-url-handler.desktop
 
-  # copy extensions
   ${pkgs.lib.concatMapStringsSep "\n" (ext: ''
     mkdir -p "$out/libraries/${ext.name}"
     cp -r "${ext.path}/"* "$out/libraries/${ext.name}/"
   '') fetchedExtensions}
 
-  # wrap OpenSCAD binary
-  ${pkgs.makeWrapper} ${pkgs.openscad}/bin/${pkgs.openscad.meta.mainProgram} \
+  makeWrapper ${pkgs.openscad}/bin/${pkgs.openscad.meta.mainProgram} \
     $out/bin/${pkgs.openscad.meta.mainProgram} --set OPENSCADPATH $out/libraries
 ''
