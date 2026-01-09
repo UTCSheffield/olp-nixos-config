4a0d1876c53340c976e86c31d97a6c00373429b2
iLikeToCode <archie@archiesbytes.xyz>
2026-01-09 22:02:17 +0000

    add timer to combat users from external source

--- programs/vscode-with-extensions.nix
+++ programs/vscode-with-extensions.nix
@@ -31,5 +31,34 @@ in
           chown -R "$user:users" "$EXT_DIR"
       done
     '';
+
+    systemd.services.vscode-extensions-sync = {
+      description = "Ensure VSCode extensions for all users";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = pkgs.writeShellScript "vscode-ext-sync" ''
+          for user in $(getent passwd | awk -F: '$3 >= 1000 {print $1}'); do
+            HOME_DIR=$(getent passwd "$user" | cut -d: -f6)
+            [ -d "$HOME_DIR" ] || continue
+
+            EXT_DIR="$HOME_DIR/.vscode/extensions"
+            mkdir -p "$EXT_DIR"
+
+            for ext in ${lib.concatMapStringsSep " " toString config.vscode.extensions}; do
+              ln -sf "$ext/share/vscode/extensions/"* "$EXT_DIR/"
+            done
+
+            chown -R "$user:users" "$EXT_DIR"
+          done
+        '';
+      };
+    };
+    systemd.timers.vscode-extensions-sync = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "2min";
+        OnUnitActiveSec = "20min";
+      };
+    };
   };
 }
