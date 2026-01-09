38267a3efa55f0e423f1eb00febdc312769ca8f5
iLikeToCode <archie@archiesbytes.xyz>
2026-01-09 17:32:13 +0000

    openscad: remove unnecessary option

--- programs/openscad-with-extensions.nix
+++ programs/openscad-with-extensions.nix
@@ -2,7 +2,7 @@
 
 let
   fetchedExtensions = map (ext: {
-    name = ext.name;
+    repo = ext.repo;
     path = pkgs.fetchFromGitHub {
       inherit (ext) owner repo rev sha256;
     };
@@ -15,13 +15,13 @@ in pkgs.runCommand "openscad-with-extensions" {
 } ''
   mkdir -p $out/bin $out/libraries $out/share/pixmaps $out/share/applications
 
-  ln -sT ${pkgs.openscad}/share/pixmaps/vs${pkgs.openscad.meta.mainProgram}.png $out/share/pixmaps/vs${pkgs.openscad.meta.mainProgram}.png
+  ln -sT ${pkgs.openscad}/share/pixmaps/vs${pkgs.openscad.meta.mainProgram}.png $out/share/pixmaps/${pkgs.openscad.meta.mainProgram}.png
   ln -sT ${pkgs.openscad}/share/applications/${pkgs.openscad.meta.mainProgram}.desktop $out/share/applications/${pkgs.openscad.meta.mainProgram}.desktop
   ln -sT ${pkgs.openscad}/share/applications/${pkgs.openscad.meta.mainProgram}-url-handler.desktop $out/share/applications/${pkgs.openscad.meta.mainProgram}-url-handler.desktop
 
   ${pkgs.lib.concatMapStringsSep "\n" (ext: ''
-    mkdir -p "$out/libraries/${ext.name}"
-    cp -r "${ext.path}/"* "$out/libraries/${ext.name}/"
+    mkdir -p "$out/libraries/${ext.repo}"
+    cp -r "${ext.path}/"* "$out/libraries/${ext.repo}/"
   '') fetchedExtensions}
 
   makeWrapper ${pkgs.openscad}/bin/${pkgs.openscad.meta.mainProgram} \
--- programs/openscad.nix
+++ programs/openscad.nix
@@ -9,7 +9,6 @@
           repo = "BOSL2";
           rev = "6ce86cd8a4a29a87444052dffb8704c2d90fd5a4";
           sha256 = "sha256-eO1DV/xrC2PuolSX4nM8t6B4F1hDQjjuqVH20hiUdh4=";
-          name = "BOSL2";
         }
       ];
     })
