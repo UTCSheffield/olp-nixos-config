7e7dfede841d8ff625a0e455bf87bd9ae24421d7
iLikeToCode <archie@archiesbytes.xyz>
2026-01-11 16:06:56 +0000

    add pam_oauth2_device

--- programs/packages/pam_oauth2_device.nix
+++ programs/packages/pam_oauth2_device.nix
@@ -5,12 +5,11 @@ let
     pname = "pam_oauth2_device";
     version = "1.0.0";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "ICS-MU";
-      repo = "pam_oauth2_device";
-      rev = "master";
-      sha256 = "sha256-y9utniTxnE1gEq3CcO2s2PbIyahaJphmA9dSMJWdijo=";
-    };
+    src = builtins.toPath ./pam_oauth2_device;
+
+    unpackPhase = "
+      cp -r $src/* .
+    ";
 
     nativeBuildInputs = [ pkgs.gnumake ];
     buildInputs = [ pkgs.linux-pam pkgs.curl.dev pkgs.openldap.dev ];
@@ -29,9 +28,19 @@ in
   config = {
     environment.systemPackages = [ pamOauth2 ];
 
-    environment.etc."pam_oauth2_device/config.json".source = builtins.toPath ./pam_oauth2_device.json;
+    environment.etc."pam_oauth2_device/config.json".source = builtins.toPath ./pam_oauth2_device/pam_oauth2_device.json;
 
     security.pam.services.login.rules.auth.settings = {
+        order = 10;
+        control = "sufficient";
+        modulePath = "${pamOauth2}/lib/security/pam_oauth2_device.so";
+    };
+    security.pam.services.login.rules.account.settings = {
+        order = 10;
+        control = "sufficient";
+        modulePath = "${pamOauth2}/lib/security/pam_oauth2_device.so";
+    };
+    security.pam.services.login.rules.session.settings = {
         order = 10;
         control = "sufficient";
         modulePath = "${pamOauth2}/lib/security/pam_oauth2_device.so";
