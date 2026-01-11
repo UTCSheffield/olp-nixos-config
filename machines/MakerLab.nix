8bfc7bd88704a8d8a8ac56b3f53125a0fc844fb0
iLikeToCode <archie@archiesbytes.xyz>
2026-01-11 18:47:39 +0000

    try with ly

--- machines/MakerLab.nix
+++ machines/MakerLab.nix
@@ -42,7 +42,7 @@
     gh
   ];
 
-  services.displayManager.gdm.enable = true;
+  services.displayManager.ly.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.xserver = {
     enable = true;
