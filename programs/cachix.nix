7198e2840a2356a2c0f92e980325f5f4ccf3d2e7
iLikeToCode <archie@archiesbytes.xyz>
2026-01-19 22:19:58 +0000

    add himmelblau cachix

--- programs/cachix.nix
+++ programs/cachix.nix
@@ -12,9 +12,11 @@
     settings = {
       substituters = [
         "https://utcsheffield.cachix.org"
+        "https://himmelblau.cachix.org"
       ];
       trusted-public-keys = [
         "utcsheffield.cachix.org-1:JlnNbGhsj00NjGND1yb6SHRoM6JO2HjgrbhpqwAp8xo="
+        "himmelblau.cachix.org-1:yu8mq/NIBYsZHWzo4SOge97gpf02qugdZFT/JdRkswc="
       ];
     };
   };
