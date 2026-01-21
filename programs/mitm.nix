4761d66f6ccf3e387a8c840b089c49f977183332
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-21 20:44:26 +0000

    Update MITM proxy URLs to new IP address

--- programs/mitm.nix
+++ programs/mitm.nix
@@ -2,7 +2,7 @@
 
 let
   mitmCert = pkgs.fetchurl {
-    url = "http://192.168.5.229:8081/mitmproxy-ca-cert.pem";
+    url = "http://192.168.5.157:8081/mitmproxy-ca-cert.pem";
     hash = "084k5c1c0mi5i1y1pny3y0q75hh1njqa2ph2jxaz504mwyra89rh";
   };
 in
@@ -12,7 +12,7 @@ in
   ];
 
   environment.sessionVariables = {
-    http_proxy  = "http://192.168.5.229:8080";
-    https_proxy = "http://192.168.5.229:8080";
+    http_proxy  = "http://192.168.5.157:8080";
+    https_proxy = "http://192.168.5.157:8080";
   };
 }
