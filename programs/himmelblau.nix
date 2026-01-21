fa113593287bf34b3a38a92014d5497c3dbebd61
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-21 13:24:36 +0000

    Update OIDC issuer URL and app ID in configuration

--- programs/himmelblau.nix
+++ programs/himmelblau.nix
@@ -5,8 +5,8 @@
     services.himmelblau = {
         enable = true;
         settings = {
-            oidc_issuer_url="tbc";
-            app_id="tbc";
+            oidc_issuer_url="https://dev-a66o74ww13tx0ori.uk.auth0.com/";
+            app_id="ucjoleQifb1ifdCYLXeojZqcwgx7BzFF";
             shell = "/run/current-system/sw/bin/bash";
         };
     };
