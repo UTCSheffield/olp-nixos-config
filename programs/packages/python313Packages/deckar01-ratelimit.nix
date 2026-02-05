3da856ddbd52b1263fe817715d7310ec2d882de5
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-02-05 08:03:33 +0000

    add deckar01-ratelimit

--- programs/packages/python313Packages/deckar01-ratelimit.nix
+++ programs/packages/python313Packages/deckar01-ratelimit.nix
@@ -5,25 +5,21 @@
   ...
 }:
 
-let
-  deckar01-ratelimit = pkgs.callPackage ./deckar01-ratelimit.nix {};
-in
 buildPythonPackage rec {
-  pname = "wordhoard";
-  version = "1.5.5";
+  pname = "deckar01-ratelimit";
+  version = "3.0.2";
   pyproject = true;
 
   src = fetchFromGitHub {
-    owner = "johnbumgarner";
-    repo = "wordhoard";
+    owner = "deckar01";
+    repo = "ratelimit";
     rev = version;
-    sha256 = "sha256-fj5GXmmH23Gpsnib4EegI9Nd4WU7oVKyoQZAMv2XIeg=";
+    sha256 = "sha256-2I0G2b6ngL2LEgtWruf2JNmvfAdOCAViqhZzxVdq4Nw=";
   };
 
   build-system = with pkgs.python313Packages; [ setuptools ];
 
   propagatedBuildInputs = with pkgs.python313Packages; [
-    deckar01-ratelimit
     backoff
     beautifulsoup4
     certifi
