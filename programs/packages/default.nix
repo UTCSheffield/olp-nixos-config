6bac3afe6f54208037c71e8a6dc0e97834090755
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-02-05 08:06:05 +0000

    fix flake check

--- programs/packages/default.nix
+++ programs/packages/default.nix
@@ -1,9 +1,7 @@
 { pkgs, ... }:
 
 {
-    python313Packages = {
-        solidpython2 = pkgs.callPackage ./python313Packages/solidpython2.nix {};
-        wordhoard = pkgs.callPackage ./python313Packages/wordhoard.nix {};
-        deckar01-ratelimit = pkgs.callPackage ./python313Packages/deckar01-ratelimit.nix {};
-    };
+    solidpython2 = pkgs.callPackage ./python313Packages/solidpython2.nix {};
+    wordhoard = pkgs.callPackage ./python313Packages/wordhoard.nix {};
+    deckar01-ratelimit = pkgs.callPackage ./python313Packages/deckar01-ratelimit.nix {};
 }
