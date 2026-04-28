d6ad7e888d7ceb2afe220ed528fe76fa0111dab3
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-04-28 15:19:04 +0000

    app mode exam timer

--- machines/exam-timer.nix
+++ machines/exam-timer.nix
@@ -13,13 +13,11 @@
   services.cage = {
     enable = true;
     user = "kioskuser";
-    program = "${pkgs.chromium}/bin/chromium --kiosk https://utcsheffield.github.io/UTC-Exam-Timer-2/web/timer.html";
+    program = "${pkgs.chromium}/bin/chromium --kiosk --app https://utcsheffield.github.io/UTC-Exam-Timer-2/web/timer.html";
   };
 
   users.users.kioskuser = {
     isNormalUser = true;
     home = "/home/kioskuser";
   };
-
-  services.getty.autologinUser = "kioskuser";
 }
