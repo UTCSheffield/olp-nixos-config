5ed4a14fa9973da92dd929df0509b9d571d9fc94
iLikeToCode <archie@archiesbytes.xyz>
2026-04-29 19:45:00 +0100

    exam-timer: move to x11 and make screens duplicate

--- machines/exam-timer.nix
+++ machines/exam-timer.nix
@@ -7,27 +7,74 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    git
+    chromium
+    openbox
+
+    xorg.xorgserver
+    xorg.xinit
+    xorg.xrandr
+    xorg.xset
   ];
-  
+
   networking.networkmanager.enable = true;
 
-  services.xserver.displayManager.sessionCommands = ''
-    ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --same-as eDP-1
+  services.xserver.enable = true;
+
+  services.xserver.windowManager.openbox.enable = false;
+
+  services.greetd = {
+    enable = true;
+
+    settings.default_session = {
+      user = "kioskuser";
+      command = "startx";
+    };
+  };
+
+  environment.etc."xdg/openbox/rc.xml".text = ''
+  <openbox_config>
+    <applications>
+      <application class="*">
+        <decor>no</decor>
+        <maximized>yes</maximized>
+      </application>
+    </applications>
+  </openbox_config>
   '';
 
-  services.cage = {
-    enable = true;
-    user = "kioskuser";
-    program = ''
-      ${pkgs.chromium}/bin/chromium \
-        --app=https://utcsheffield.github.io/UTC-Exam-Timer-2/web/timer.html \
-        --start-fullscreen \
-        --no-first-run \
-        --incognito \
-        --disable-pinch
-    '';
-  };
+  environment.etc."X11/xinit/xinitrc".text = ''
+    xset s off
+    xset -dpms
+    xset s noblank
+
+    sleep 2
+
+    # start window manager FIRST
+    openbox-session &
+    sleep 2
+
+    # detect primary output
+    PRIMARY=$(xrandr | grep " connected" | cut -d" " -f1 | head -n1)
+
+    # mirror all outputs to primary
+    for out in $(xrandr | grep " connected" | cut -d" " -f1); do
+      xrandr --output "$out" --auto
+      if [ "$out" != "$PRIMARY" ]; then
+        xrandr --output "$out" --same-as "$PRIMARY"
+      fi
+    done
+
+    # launch Chromium in APP MODE (NOT kiosk)
+    chromium \
+    --app=https://utcsheffield.github.io/UTC-Exam-Timer-2/web/timer.html \
+    --start-maximized \
+    --no-first-run \
+    --disable-pinch \
+    --disable-infobars \
+    --noerrdialogs \
+    --disable-session-crashed-bubble \
+    --ozone-platform=x11
+  '';
 
   users.users.kioskuser = {
     isNormalUser = true;
