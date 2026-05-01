851e791fdacbcdef5d7d2edf4e5fedffaaa64df2
iLikeToCode <archie@archiesbytes.xyz>
2026-05-01 11:12:56 +0100

    new format of file

--- machines/kiosk.nix
+++ machines/kiosk.nix
@@ -13,22 +13,24 @@
   };
 
   system.stateVersion = "25.11";
-}
-// lib.mkIf (config.specialisation != {}) {
-  systemd.services.update-tool = lib.mkForce {
-    enable = true;
-    wantedBy = [ "multi-user.target" ];
-    after = [ "multi-user.target" ];
-    requires = [ "network.target" ];
 
-    serviceConfig = {
-      ExecStart =
-        "${pkgs.callPackage ../update-tool/update-tool.nix { }}/bin/client";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      TTYPath = "/dev/tty1";
-    };
-  };
+  systemd.services.update-tool = lib.mkIf (config.specialisation != {}) (
+    lib.mkForce {
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+      after = [ "multi-user.target" ];
+      requires = [ "network.target" ];
 
-  systemd.services."autovt@tty1".enable = false;
+      serviceConfig = {
+        ExecStart =
+          "${pkgs.callPackage ../update-tool/update-tool.nix { }}/bin/client";
+        StandardInput = "tty";
+        StandardOutput = "tty";
+        TTYPath = "/dev/tty1";
+      };
+    }
+  );
+
+  systemd.services."autovt@tty1".enable =
+    lib.mkIf (config.specialisation != {}) false;
 }
